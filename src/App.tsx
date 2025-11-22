9074a66fd9b8776ff574a01d786db0fbd52880fb
OfekAmar <ofek.ama@gmail.com>
2025-11-22 21:05:42 +0200

    Starting from base search + results basic UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,68 @@
-import React from 'react';
+import React, { use } from 'react';
+import { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
+import { Book , Tag} from "./types";
 
 function App() {
+
+  const [books, setBooks] = useState<Book[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>("");
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [selectedTag, setSelectedTag] = useState<Tag | null>(null);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("/books.json")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to load books.json");
+        }
+        return res.json()
+      })
+      .then((data) => {
+        setBooks(data);
+        setError("");
+      })
+      .catch((err) => {
+        setError(err.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p style={{ color: 'red' }}> Error : {error}  </p>;
+  }
+
+  const filteredBooks = books.filter((book) => {
+    const lowercaseSearchTerm = searchTerm.toLowerCase();
+    const titleRes = book.title.toLowerCase().includes(lowercaseSearchTerm);
+    const authorRes = book.author.toLowerCase().includes(lowercaseSearchTerm);
+    return titleRes || authorRes;
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>  Library Explorer </h1>
+      <div>
+        <label htmlFor='search'> Search by title or author: </label>
+        <input id='search' type='text' value={searchTerm} onChange={(event) => setSearchTerm(event.target.value)}></input>
+      </div>
+      <ul>
+        {filteredBooks.map((book) => (
+          <li key={book.id}>
+            <strong>{book.title}</strong> - {book.author}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
